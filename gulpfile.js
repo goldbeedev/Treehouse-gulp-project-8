e809a1973db2a091b1c44de6d48ec88ff1b8af96
Justin Goldby <goldbee500@gmail.com>
2016-09-29 23:43:56 -0700

    Treehouse Project 8 first and final commit
    Ready for grading, coded to meet expectations

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+"use strict";
+
+//require modules
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+     del = require('del'),
+imagemin = require('gulp-imagemin'),
+webserver = require('gulp-connect'),
+runSequence = require('run-sequence');
+
+var options = {
+  src: 'src',
+  dist: 'dist'
+
+};
+
+//concat the scripts
+gulp.task("concatScripts", function() {
+    return gulp.src([
+        'js/global.js',
+        'js/circle/autogrow.js',
+        'js/circle/circle.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('js'));
+});
+
+//minify the scripts
+gulp.task("scripts", ["concatScripts"], function() {
+  return gulp.src("js/global.js")
+  	.pipe(maps.init())
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest(options.dist + '/scripts'));
+});
+
+//minify the css 
+gulp.task('styles', function() {
+  return gulp.src(["sass/**.scss"])
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest(options.dist + '/styles'))
+
+
+});
+
+//clean task to clean up the folders before the build runs
+gulp.task('clean', function() {
+  return del(['dist/content/**', 'dist/scripts/**', 'dist/styles/**']);
+
+});
+
+//minifiy images, dest dist/content
+gulp.task('images', function() {
+  return gulp.src('images/*')
+             .pipe(imagemin())
+             .pipe(gulp.dest('dist/content'))
+});
+
+//watch function for any javascript file changes
+gulp.task('watch', function() {
+  gulp.watch(['js/**/*.js'], ['scripts']);
+});
+
+//use gulp connect to start a web server that uses live reload to reload the page with any changes
+gulp.task('webserver', function() {
+  webserver.server({
+    livereload: true
+  });
+});
+
+//set up the build task to call the other tasks, with clean completing first.
+gulp.task('build', function() {
+  runSequence('clean', ['scripts', 'styles', 'images']);
+});
+
+//set up the default gulp task to have build as a dependency.
+gulp.task('default', ['build']);
+
+//set up the serve task to build and serve the project while using watch for any changes
+gulp.task('serve', ['build', 'webserver', 'watch']);
+
+
